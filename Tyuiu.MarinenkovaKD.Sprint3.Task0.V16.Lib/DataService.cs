5a1c1530760e674d53b81512a59edd84f3ca7011
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-10-19 20:29:38 +0500

    Task0

--- Tyuiu.MarinenkovaKD.Sprint3.Task0.V16.Lib/DataService.cs
+++ Tyuiu.MarinenkovaKD.Sprint3.Task0.V16.Lib/DataService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.MarinenkovaKD.Sprint3.Task0.V16.Lib
--- Tyuiu.MarinenkovaKD.Sprint3.Task0.V16/Program.cs
+++ Tyuiu.MarinenkovaKD.Sprint3.Task0.V16/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Tyuiu.MarinenkovaKD.Sprint3.Task0.V16.Lib;
 
 namespace Tyuiu.MarinenkovaKD.Sprint3.Task0.V16
