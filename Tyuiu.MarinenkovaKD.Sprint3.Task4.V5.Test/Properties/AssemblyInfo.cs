c842f682c714ee1ce663e3c577f146b484dedc6b
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-10-27 19:38:53 +0500

    Task4

---
+++ Tyuiu.MarinenkovaKD.Sprint3.Task4.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MarinenkovaKD.Sprint3.Task4.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MarinenkovaKD.Sprint3.Task4.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c097076-676f-410a-81d6-a74f86dbf910")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
